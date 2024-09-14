a8132e831b9e909583f6dafef83e09272434fef9
Gfpila <gustavofaustino925@gmail.com>
2024-09-13 21:21:51 -0300

    Atualizacao BackEnd

---
+++ AtividadeMobile/DataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using AtividadeMobile.Models;
+using SQLite;
+
+namespace AtividadeMobile
+{
+    class DataBaseContext
+    {
+        private const string DB_NAME = "cadastros.db3";
+        private readonly string dbPath = Path.Combine(FileSystem.AppDataDirectory, DB_NAME);
+        public SQLiteConnection connection { get; }
+
+        public DataBaseContext()
+        {
+           connection = new SQLiteConnection(dbPath);
+           connection.CreateTable<Cadastro>();
+        }
+
+    }
+}
---
+++ AtividadeMobile/Services/CadastroServices.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using AtividadeMobile.Models;
+using AtividadeMobile.Repositories;
+
+namespace AtividadeMobile.Services
+{
+    public class CadastroServices
+    {
+        private readonly CadastroRepository _cadastroRepository;
+
+        public CadastroServices()
+        {
+            _cadastroRepository = new CadastroRepository();
+        }
+
+        public List<Cadastro> ObterCadastros()
+        {
+            return _cadastroRepository.Read();
+        }
+
+        public bool Cadastrar(Cadastro cadastro)
+        {
+            try
+            {
+                _cadastroRepository.Create(cadastro);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public bool AtualizarCadastro(Cadastro cadastro)
+        {
+            try
+            {
+                _cadastroRepository.Update(cadastro);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public bool DeletarCadastro(Cadastro cadastro)
+        {
+            try
+            {
+                _cadastroRepository.Delete(cadastro);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public bool Login(string email, string senha)
+        {
+            var cadastros = ObterCadastros();
+            foreach (var cadastro in cadastros)
+            {
+                if (cadastro.Email == email && cadastro.Senha == senha)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
